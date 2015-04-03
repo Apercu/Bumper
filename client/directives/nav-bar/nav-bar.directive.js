419e4e7337a03258923b37c50f51fe9a42b28e0a
Aperçu <bgronon@gmail.com>
2015-04-04 01:59:53 +0200

    style(core): login in navbar

--- client/directives/nav-bar/nav-bar.directive.js
+++ client/directives/nav-bar/nav-bar.directive.js
@@ -1,10 +1,22 @@
 'use strict';
 
 angular.module('bumper')
-  .directive('navBar', function () {
+  .directive('navBar', function ($window) {
     return {
       restrict: 'E',
       replace: true,
-      templateUrl: 'directives/nav-bar/nav-bar.html'
+      templateUrl: 'directives/nav-bar/nav-bar.html',
+      link: function (scope) {
+
+        scope.nav = {
+          redirecting: false
+        };
+
+        scope.login = function () {
+          scope.nav.redirecting = true;
+          $window.location.href = '/auth/github';
+        }
+
+      }
     };
   });

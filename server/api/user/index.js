3285e189272630d03ffbc5dff9fb5551a99bdfcf
meriadec <meriadec.pillet@gmail.com>
2015-04-04 00:24:47 +0200

    refactor(user): remove unused code

--- server/api/user/index.js
+++ server/api/user/index.js
@@ -6,6 +6,5 @@ var controller = require('./user.controller');
 var auth = require('../../auth/auth.service');
 
 router.get('/me', auth.isAuthenticated(), controller.getMe);
-router.post('/', controller.create);
 
 module.exports = router;

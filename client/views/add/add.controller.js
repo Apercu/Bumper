b7d2f864be2f898b917fc160b42c0e100e561293
Balthazar Gronon <bgronon@gmail.com>
2015-04-04 23:33:52 +0000

    style(add): animate add and remove buttons

--- client/views/add/add.controller.js
+++ client/views/add/add.controller.js
@@ -24,21 +24,25 @@ angular.module('bumper')
       },
 
       addRepo: function (repo) {
+        repo.adding = true;
         Repo.addFromGithub(repo)
           .then(function (rep) {
             repo.addedToBumper = true;
             repo.bumperId = rep._id;
           })
-          .catch(function (err) { console.log(err); });
+          .catch(function (err) { console.log(err); })
+          .finally(function () { repo.adding = false; });
       },
 
       removeRepo: function (repo) {
+        repo.removing = true;
         Repo.remove(repo.bumperId)
           .then(function () {
             repo.addedToBumper = false;
             repo.bumperId = null;
           })
-          .catch(function (err) { console.log(err); });
+          .catch(function (err) { console.log(err); })
+          .finally(function () { repo.removing = false; });
       }
 
     });

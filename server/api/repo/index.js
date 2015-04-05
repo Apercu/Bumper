97790105b0ca490cea6882fe1a540a58db37d2bc
Balthazar Gronon <bgronon@gmail.com>
2015-04-05 02:10:34 +0000

    fix(api): middlewares of repo routes

--- server/api/repo/index.js
+++ server/api/repo/index.js
@@ -3,9 +3,10 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('./repo.controller');
+var auth = require('../../auth/auth.service');
 
-router.get('/', controller.index);
-router.post('/', controller.create);
-router.delete('/:id', controller.destroy);
+router.get('/', auth.isAuthenticated(), controller.index);
+router.post('/', auth.isAuthenticated(), controller.create);
+router.delete('/:id', auth.isAuthenticated(), controller.destroy);
 
 module.exports = router;
